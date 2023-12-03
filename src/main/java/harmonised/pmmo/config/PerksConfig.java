c1466aae8e4f65511a03dafecb74c850a672453e
Justin Tamblyn <just1689@gmail.com>
2023-12-03 11:15:38 +0200

    remove speed perks

--- src/main/java/harmonised/pmmo/config/PerksConfig.java
+++ src/main/java/harmonised/pmmo/config/PerksConfig.java
@@ -150,11 +150,14 @@ public class PerksConfig {
 		bodyList.clear();
 
 		//=====================SPRINTING================================
-		bodyList.add(TagBuilder.start().withString("perk", "pmmo:effect").withString(APIUtils.SKILLNAME, "agility")
-				.withString("effect", "minecraft:speed")
-				.withInt(APIUtils.MODIFIER, 2)
-				.withInt(APIUtils.PER_LEVEL, 1)
-				.withInt(APIUtils.DURATION, 20).build());
+//		bodyList.add(
+//				TagBuilder.start()
+//				.withString("perk", "pmmo:effect")
+//				.withString(APIUtils.SKILLNAME, "agility")
+//				.withString("effect", "minecraft:speed")
+//				.withInt(APIUtils.MODIFIER, 2)
+//				.withInt(APIUtils.PER_LEVEL, 1)
+//				.withInt(APIUtils.DURATION, 20).build());
 		defaultSettings.put(EventType.SPRINTING, new ArrayList<>(bodyList));
 	}
 	
